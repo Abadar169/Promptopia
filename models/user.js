1b4f2f03989c4536e879c6d1accd57b87903dc89
Your Name <you@example.com>
2023-05-29 16:18:04 +0530

    adding the final project

---
+++ models/user.js
@@ -0,0 +1,23 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  },
+  image: {
+    type: String,
+  }
+});
+
+//we have to check if model already exist 1)look in models.User if it exist
+                                        //2)if not then create new 
+const User = models.User || model("User", UserSchema);
+
+export default User;
